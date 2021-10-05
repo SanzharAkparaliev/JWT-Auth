a4f0a6ecbbc0c27eb327eaa9ee177460074dae03
SanzharAkparaliev <taalaibekuulusanzhar@gmail.com>
2021-10-05 22:35:14 +0600

    Hashing Passwords

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const {isEmail} = require('validator')
+const bcrypt = require('bcrypt')
 const userSchema = new mongoose.Schema({
     email:{
         type:String,
@@ -16,14 +17,10 @@ const userSchema = new mongoose.Schema({
     }
 })
 
-//fire a function after doc saved to db
-userSchema.post('save',function(doc,next){
-    console.log('new user was created  & saved',doc)
-    next()
-})
-
-userSchema.pre('save',function(next){
-    console.log('user about to be created & saved',this)
+// fire a function before doc saved to db
+userSchema.pre('save',async function(next){
+    const salt = await bcrypt.genSalt()
+    this.password = await bcrypt.hash(this.password,salt)
     next()
 })
 
