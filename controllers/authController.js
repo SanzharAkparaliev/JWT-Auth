0f7492223fd3dd014ae6b395138d03deec623804
SanzharAkparaliev <taalaibekuulusanzhar@gmail.com>
2021-10-05 16:47:23 +0600

    User model

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+const User = require('../models/User')
 module.exports.signup_get = (req,res) => {
     res.render('signup')
 }
@@ -6,10 +8,19 @@ module.exports.login_get = (req,res) => {
     res.render('login')
 }
 
-module.exports.signup_post = (req,res) => {
+module.exports.signup_post = async (req,res) => {
+    const {email,password } = req.body
+    try {
+        const user = await User.create({email,password})
+        res.status(201).json(user)
+    } catch (err) {
+        console.log(err)
+        res.status(400).send('error, user not created')
+    }
     res.send('new signup')
 }
 
 module.exports.login_post = (req,res) => {
+    console.log(req.body)
     res.send('user login')
 }
--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -5,7 +5,7 @@ const authController = require('../controllers/authController')
 router.get('/signup',authController.signup_get)
 router.post('/signup',authController.signup_post)
 router.get('/login',authController.login_get)
-router.post('/signup',authController.login_post)
+router.post('/login',authController.login_post)
 
 
 module.exports = router;
