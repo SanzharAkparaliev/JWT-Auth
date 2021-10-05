73bad652934ad29b3f0aa7c3a3fdf22ffb1e50d8
SanzharAkparaliev <taalaibekuulusanzhar@gmail.com>
2021-10-05 18:26:18 +0600

    Mongoose Validation

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,26 @@
 const mongoose = require('mongoose');
 const User = require('../models/User')
+
+//handle errors
+const handleErrors = (err) => {
+    console.log(err.message,err.code)
+    let errors = {email:'',password:''}
+
+    //duplicate error code
+    if(err.code === 11000){
+        errors.email = 'that email is already registered'
+        return errors
+    }
+
+    //validation errors
+    if(err.message.includes('user validation failed')){
+        Object.values(err.errors).forEach(({properties}) => {
+            errors[properties.path] = properties.message
+        })
+    }
+    return errors;
+}
+
 module.exports.signup_get = (req,res) => {
     res.render('signup')
 }
@@ -14,10 +35,9 @@ module.exports.signup_post = async (req,res) => {
         const user = await User.create({email,password})
         res.status(201).json(user)
     } catch (err) {
-        console.log(err)
-        res.status(400).send('error, user not created')
+        const errors = handleErrors(err)
+        res.status(400).json({errors})
     }
-    res.send('new signup')
 }
 
 module.exports.login_post = (req,res) => {
